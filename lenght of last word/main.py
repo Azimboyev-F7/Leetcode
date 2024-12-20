042fc1a3b239a13bd55b1fd4a7ed3451fca3174d
Azimboyev-F7 <azimboyevfaxriyor.official@gmail.com>
2024-12-20 10:19:58 +0500

    change

---
+++ lenght of last word/main.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        if s.split():
+            return len(s.split()[-1])
+        return 0
+
+a = Solution()
+
+print(a.lengthOfLastWord("Hello World"))
